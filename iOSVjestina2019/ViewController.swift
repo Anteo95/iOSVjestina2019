90bdea09cfd8b8873df40e58a9c3e0b560698945
anteo <anteo.ivankov@gmail.com>
2019-03-29 22:32:26 +0100

    Initial Commit

---
+++ iOSVjestina2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSVjestina2019
+//
+//  Created by Anteo Ivankov on 29/03/2019.
+//  Copyright © 2019 Anteo Ivankov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
